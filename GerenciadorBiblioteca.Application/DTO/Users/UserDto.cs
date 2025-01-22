04c8e261271c6f2dc414385ad1ecb3c982972881
Rodrigo Santos <rodrigocbs914@gmail.com>
2025-01-21 21:51:52 -0300

    refactor: adicionando a lista de empréstimos do usuário ao seu DTO.

--- GerenciadorBiblioteca.Application/DTO/Users/UserDto.cs
+++ GerenciadorBiblioteca.Application/DTO/Users/UserDto.cs
@@ -4,14 +4,17 @@ namespace GerenciadorBiblioteca.Application.DTOs.Users
 {
     public class UserDto
     {
-        public UserDto(string name, string email)
+        public UserDto(string name, string email, List<Loan> bookLoans)
         {
             Name = name;
             Email = email;
+            BookLoans = bookLoans;
         }
         public string Name { get; private set; }
         public string Email { get; private set; }
 
-        public static UserDto FromEntity(User user) => new UserDto(user.Name, user.Email);
+        public List<Loan> BookLoans { get; private set; }
+
+        public static UserDto FromEntity(User user) => new UserDto(user.Name, user.Email, user.BookLoans);
     }
 }
